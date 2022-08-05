92078eaf30fec65c9f1f884ef7f3e368f64f2431
Mohand <mohand_abchiche@live.fr>
2022-08-05 15:42:52 -0700

    Setup libraries needed for project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './node_modules/flowbite/**/*.js',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    './node_modules/flowbite-datepicker/**/*.js'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      height:{
+        100 : '30rem',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    // require('flowbite/plugin')
+  ],
 }
