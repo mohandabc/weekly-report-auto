ab32b3315c2ed23a705a9858a3b64c3921797ce5
Mohand <mohand_abchiche@live.fr>
2022-08-07 15:59:56 +0100

    edit getData function to rcv a route and parameters

--- src/api/getData.js
+++ src/api/getData.js
@@ -1,8 +1,9 @@
-const API_URL = 'http://10.171.59.82:8069/reports/daily_report';
+const API_URL = 'http://10.171.59.82:8069/';
 
-export const getData = ()=>{
+export const getData = (route, params)=>{
+
     //@@@@ IMPORTANT :  dbfilter setting needs to be set to a database for the REST interface to work.
-    fetch(API_URL, {
+    fetch(`${API_URL}${route}`, {
       method: 'POST',
       body: JSON.stringify({"jsonrpc":"2.0","params":{}}),
       // mode: 'no-cors',
