7f6f6ffcf87e4cbb969bee5ed3d615b75fc2145c
Mohand <mohand_abchiche@live.fr>
2022-08-11 16:29:08 +0100

    ConfigBar takes action to execute when Submit button is pressed

--- src/components/ConfigBar/ConfigBar.jsx
+++ src/components/ConfigBar/ConfigBar.jsx
@@ -1,30 +1,23 @@
 import { DateSelector, ActionButton} from '..';
-import { getData } from '../../api/getData';
 
-import gear from '../../assets/gear.svg';
-import { generatePDF } from '../../utils';
 
-import {chartsToPrintState, dateStartEndState} from '../../shared/globalState';
+import {dateStartEndState} from '../../shared/globalState';
 import {useRecoilValue} from 'recoil';
 
 
-export const ConfigBar = ()=>{
-    const chartsToPrint = useRecoilValue(chartsToPrintState);
+export const ConfigBar = ({configBarAction})=>{
     const dateRange = useRecoilValue(dateStartEndState);
 
     const params = {
-        dates : dateRange
+        dates : '2021-09-01 - 2021-09-30',
     }
-
-    const test = ()=>console.log('helo');
 
     return (
     <div className='flex '>
         <div className='flex-initial w-64'>
             <DateSelector></DateSelector>
         </div>
-        <ActionButton text="Submit" action={getData} args={['reports/daily_report', params]}></ActionButton>
-        <ActionButton text="PDF" action={generatePDF} args={[chartsToPrint]}></ActionButton>
+        <ActionButton text="Submit" action={configBarAction} args={[params]}></ActionButton>
     </div>
     );
 }
