91378c21ffbabddd285f0d46c21a640b0d45f4e5
Mohand <mohand_abchiche@live.fr>
2022-08-11 16:16:51 +0100

    possibility to print table componenents using html2canvas

--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,8 @@ import pdfFonts from "pdfmake/build/vfs_fonts";
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 
+import html2canvas from 'html2canvas';
+
 const getChartByContainerId = (id) => {
   var charts = am4core.registry.baseSprites;
   for(var i = 0; i < charts.length; i++) {
@@ -14,7 +16,7 @@ const getChartByContainerId = (id) => {
   }
 }
 
-export const generatePDF = (chartsToPrint) =>{
+export const generatePDF = (chartsToPrint, tablesToPrint) =>{
     if(chartsToPrint.length === 0){
       return;
     }
@@ -25,7 +27,9 @@ export const generatePDF = (chartsToPrint) =>{
     }
 
     let promises_list = charts.map((chart) => chart.exporting.getImage("png"));
-    promises_list = [charts[0].exporting.pdfmake, ...promises_list];
+    const nbr_charts = promises_list.length;
+    let tables_promesses = tablesToPrint.map(table => html2canvas(document.getElementById(table)));
+    promises_list = [charts[0].exporting.pdfmake, ...promises_list, ...tables_promesses];
 
     Promise.all(promises_list).then(function (res) {
         var pdfMake=res[0];
@@ -37,13 +41,23 @@ export const generatePDF = (chartsToPrint) =>{
           pageMargins: [15,20,0,10],
           content: [],  
           };
-        res.slice(1).forEach((chart)=>{
+        res.slice(1,nbr_charts+1).forEach((chart)=>{
           doc.content.push({
             image:chart,
             margin : [5,5,0,0],
             width : 400,
           })
         });
+
+        res.slice(nbr_charts+1).forEach((table)=>{
+          doc.content.push({
+            image:table.toDataURL("image/png"),
+            margin : [5,5,0,0],
+            width : 250,
+          })
+        });
+
+        
        
         pdfMake.createPdf(doc).download("daily.pdf");
     });
