31a2e28ef1aafe95dc50196d5735e09b285f0cae
Mohand <mohand_abchiche@live.fr>
2022-08-12 12:04:12 +0100

    Update generateWeeklyRepot
    - function to xport charts and table
    - function to create header of reports
    - function add a given chart to pdf

--- src/utils.js
+++ src/utils.js
@@ -2,10 +2,13 @@
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 
+
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 
 import html2canvas from 'html2canvas';
+
+import {SMARTEST_LOGO, SONATRACH_LOGO} from './constants/logos'
 
 const getChartByContainerId = (id) => {
   var charts = am4core.registry.baseSprites;
@@ -16,7 +19,67 @@ const getChartByContainerId = (id) => {
   }
 }
 
-export const generatePDF = (chartsToPrint, tablesToPrint) =>{
+const exportCharts = async (charts, tablesToPrint) => {
+    let promises_list = charts.map((chart) => chart.exporting.getImage("png"));
+    const nbr_charts = promises_list.length;
+    let tables_promesses = tablesToPrint.map(table => html2canvas(document.getElementById(table)));
+    promises_list = [...promises_list, ...tables_promesses];
+
+    return Promise.all(promises_list)
+    .then((res) => [res.slice(0, nbr_charts), res.slice(nbr_charts) ]);
+}
+
+const createHeaderPage = (doc, title, range) =>{
+  doc.content.push({
+    columns: [{
+        image: SMARTEST_LOGO,
+        margin: [0,50,0,0],
+        width: 160
+    },{
+
+        image: SONATRACH_LOGO,
+        margin: [330,40,0,0],
+        width: 40
+    }]
+  })
+
+  doc.content.push({
+    text : title,
+    fontSize : 48,
+    margin:[0,100,0,20],
+    alignment:'center',
+    color:'#444',
+    
+  });
+  doc.content.push({
+    text : `From ${range.split(" - ")[0]} To ${range.split(" - ")[1]}`,
+    fontSize : 36,
+    margin:[0,0,0,100],
+    alignment:'center',
+    color:'#444',
+    pageBreak:'after'
+  });
+
+}
+
+const addChartToPDF = (doc, title, chart) =>{
+  doc.content.push({
+    text : title,
+    fontSize:16,
+    color:'#CC5555', 
+    alignment:'center',
+  });
+  doc.content.push({
+    image:chart,
+    margin : [5,5,0,0],
+    width : 250,
+    alignment:'center',
+
+  });
+
+}
+
+export const generateWeeklyReport = (chartsToPrint, tablesToPrint, range) =>{
     if(chartsToPrint.length === 0){
       return;
     }
@@ -26,13 +89,10 @@ export const generatePDF = (chartsToPrint, tablesToPrint) =>{
       return;
     }
 
-    let promises_list = charts.map((chart) => chart.exporting.getImage("png"));
-    const nbr_charts = promises_list.length;
-    let tables_promesses = tablesToPrint.map(table => html2canvas(document.getElementById(table)));
-    promises_list = [charts[0].exporting.pdfmake, ...promises_list, ...tables_promesses];
-
-    Promise.all(promises_list).then(function (res) {
-        var pdfMake=res[0];
+    exportCharts(charts, tablesToPrint)
+    .then(response => {
+        const [exportedCharts, exportedTables] = response;    
+    
         pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
         var doc={
@@ -41,26 +101,35 @@ export const generatePDF = (chartsToPrint, tablesToPrint) =>{
           pageMargins: [15,20,0,10],
           content: [],  
           };
-        res.slice(1,nbr_charts+1).forEach((chart)=>{
-          doc.content.push({
-            image:chart,
-            margin : [5,5,0,0],
-            width : 400,
-          })
-        });
 
-        res.slice(nbr_charts+1).forEach((table)=>{
-          doc.content.push({
-            image:table.toDataURL("image/png"),
-            margin : [5,5,0,0],
-            width : 250,
-          })
-        });
+          createHeaderPage(doc, "BO Weekly Report", range);
+          addChartToPDF(doc, "Rig Box, Maintenance", exportedCharts[0]);
+          addChartToPDF(doc, "NDJ Jobs", exportedCharts[1]);
+          addChartToPDF(doc, "Cementing Job Transmission", exportedTables[0]?.toDataURL("image/png"));
+          addChartToPDF(doc, "MWD Transmission", exportedTables[1]?.toDataURL("image/png"));
+          
 
+
+        //   exportedCharts.forEach((chart)=>{
+        //   doc.content.push({
+        //     image:chart,
+        //     margin : [5,5,0,0],
+        //     width : 400,
+        //   })
+        // });
+
+        // exportedTables.forEach((table)=>{
+        //   doc.content.push({
+        //     image:table.toDataURL("image/png"),
+        //     margin : [5,5,0,0],
+        //     width : 250,
+        //   })
+        // });
         
-       
         pdfMake.createPdf(doc).download("daily.pdf");
-    });
+      });
+       
+    // });
   }
 
   
