e34e89f9693fd48d0b34afb38bd10081a1410095
Mohand <mohand_abchiche@live.fr>
2022-08-12 12:03:39 +0100

    add range to generatePDF param and update api route (weekly not daily)

--- src/pages/WeeklyPage/WeeklyPage.jsx
+++ src/pages/WeeklyPage/WeeklyPage.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import {useRecoilState} from 'recoil';
-import {chartsToPrintState, tablesToPrintState, weeklyDataState} from '../../shared/globalState';
+import {useRecoilState, useRecoilValue} from 'recoil';
+import {chartsToPrintState, dateStartEndState, tablesToPrintState, weeklyDataState} from '../../shared/globalState';
 
 import { ActionButton, Chart, ConfigBar, Table} from '../../components';
 
@@ -15,6 +15,7 @@ export const WeeklyPage = () => {
     const [chartsToPrint, setChartsToPrint] = useRecoilState(chartsToPrintState);
     const [tablesToPrint, setTablesToPrint] = useRecoilState(tablesToPrintState);
     const [weeklyData, setWeeklyData] = useRecoilState(weeklyDataState);
+    const range = useRecoilValue(dateStartEndState);
 
     let chartsIds = [];
     let tablesIds = [];
@@ -34,7 +35,7 @@ export const WeeklyPage = () => {
     }
     const getWeeklyData =  (params) =>{
     
-        const path = 'reports/daily_report';
+        const path = 'reports/weekly_report';
     
         getData(path, params)
         .then(res=> {
@@ -64,18 +65,18 @@ export const WeeklyPage = () => {
                 <ActionButton className="bg-blue-500 hover:bg-blue-700 text-black font-bold text-base py-2 px-4 mx-auto float-right sticky top-5 rounded" 
                                 text="PDF" 
                                 action={generateWeeklyReport} 
-                                args={[chartsToPrint, tablesToPrint]}>
+                                args={[chartsToPrint, tablesToPrint, range]}>
                 </ActionButton>
                 <section className={`grid grid-cols-2 gap-4 place-items-center bg-slate-500 `} >
-                    <Chart title = "Rig Box Maintenance" id = {getDivId('chart')} chartData = {weeklyData['rigbox_maintenance']}  chartType="Pie"/>
-                    <Chart title = "NDJ Jobs"  id = {getDivId('chart')} chartData = {weeklyData['ndj']} chartType="ClusterBar"/>
                     <Chart title = "Rig Box Maintenance"  id = {getDivId('chart')} chartData = {weeklyData['rigbox_maintenance']} chartType="Bar"/>
+                    <Chart title = "NDJ Jobs"  id = {getDivId('chart')} chartData = {weeklyData['ndj']} chartType="ClusterBar"/>
                     <Chart title = "Global Recovery"  id = {getDivId('chart')} chartData = {weeklyData['global_recovery']} chartType="Bar"/>
                     <Chart title = "Weekly Recovery"  id = {getDivId('chart')} chartData = {weeklyData['weekly_recovery']} chartType="Bar"/>
                     <Table title = "Cementing Jobs Transmission"  id = {getDivId('table')} tableData = {weeklyData['cementing_jobs']}/>
                     <Table title = "MWD Transmission"  id = {getDivId('table')} tableData = {weeklyData['mwd_jobs']}/>
                     <Chart title = "resolved Quality MLU"  id = {getDivId('chart')} chartData = {weeklyData['resolved_quality_company']} chartType="ClusterBar"/>
                     <Chart title = "resolved Quality Rootcase"  id = {getDivId('chart')} chartData = {weeklyData['resolved_quality_rootcause']} chartType="ClusterBar"/>
+                    <Chart title = "Rig Box Maintenance" id = {getDivId('chart')} chartData = {weeklyData['rigbox_maintenance']}  chartType="Pie"/>
                 </section>
             </div>
             
