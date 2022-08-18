0b5f8e8d064ec6f94dfbccf97a212b5a3d602d90
Mohand <mohand_abchiche@live.fr>
2022-08-18 11:15:19 +0100

    add buttons for future reports in main page

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -11,13 +11,10 @@ export const MainPage = () =>{
 
                 <div className="flex flex-wrap">
 
-                    <div className="w-1/4">
-                    </div>
-
                     <div className="w-1/4 px-8">
                         <Link className="no-underline text-header" to="daily">
                             <div className="bg-gray-200 h-60 w-full p-4 text-center rounded-lg shadow-md mb-3 transform transition duration-200 ease-out hover:scale-105 hover:shadow-lg">
-                                <h3>Daily Report</h3>
+                                <h3>BO Daily Report</h3>
                                 <p>Automaticall Generates a back office daily report for a specific day</p>
                             </div>
                         </Link>
@@ -26,14 +23,29 @@ export const MainPage = () =>{
                     <div className="w-1/4 px-8 ">
                         <Link className="no-underline text-header" to="weekly">
                             <div className="bg-gray-200 h-60 w-full py-4 text-center rounded-lg shadow-md mb-3 transform transition duration-200 ease-out hover:scale-105 hover:shadow-lg">
-                                <h3>Weekly Report</h3>
+                                <h3>BO Weekly Report</h3>
                                 <p>Automaticall Generates a back office weekly report for a specific week</p>
                             </div>
                         </Link>
                     </div>
 
-                    <div className="w-1/4">
 
+                    <div className="w-1/4 px-8">
+                        <Link className="no-underline text-header" to="">
+                            <div className="bg-gray-400 h-60 w-full p-4 text-center rounded-lg shadow-md mb-3 transform transition duration-200 ease-out hover:scale-105 hover:shadow-lg">
+                                <h3>FO Daily Report</h3>
+                                <p>Automaticall Generates a front office daily report for a specific day</p>
+                            </div>
+                        </Link>
+                    </div>
+
+                    <div className="w-1/4 px-8">
+                        <Link className="no-underline text-header" to="">
+                            <div className="bg-gray-400 h-60 w-full py-4 text-center rounded-lg shadow-md mb-3 transform transition duration-200 ease-out hover:scale-105 hover:shadow-lg">
+                                <h3>FO Weekly Report</h3>
+                                <p>Automaticall Generates a front office weekly report for a specific week</p>
+                            </div>
+                        </Link>
                     </div>
 
                 </div>
