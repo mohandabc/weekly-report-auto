bfb073f7071e310bf2e657459852197a58a8fe5b
Mohand <mohand_abchiche@live.fr>
2022-08-19 16:05:25 +0100

    correct a display bug in Chart component

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -4,7 +4,7 @@ import { BarChart, PieChart, ClusteredBarChart } from './charts';
 import gear from '../../assets/gear.svg';
 
 export const Chart = ({title, id, chartData, chartType}) => {
-  const div = id.toString();
+  const divID = id.toString();
   const [type, setType] = useState(chartType);
 
   const changeType = () =>{
@@ -18,33 +18,32 @@ export const Chart = ({title, id, chartData, chartType}) => {
     }
 
     if(type === "Pie"){ 
-      chart = new PieChart(chartData, div, title).chart; 
+      chart = new PieChart(chartData, divID, title).chart; 
     }
     if(type === "Bar"){
-      chart = new BarChart(chartData, div, title).chart; 
+      chart = new BarChart(chartData, divID, title).chart; 
     }
     if(type === "ClusterBar"){
       // eslint-disable-next-line
-      chart = new ClusteredBarChart(chartData, div, title).chart; 
+      chart = new ClusteredBarChart(chartData, divID, title).chart; 
     }
   });
 
   return (
         <div className="h-120  pb-5 bg-white rounded-lg shadow w-full">
-          {
-            type==='Pie' || type === 'Bar' ? 
-            <div>
-              <img src={gear} onClick={changeType} className="px-2 py-2 w-10 hover:cursor-pointer hover:animate-spin" alt="..."></img>
-            </div>
-            : <></>
-          }
-          <div 
-            className='inline-block mx-auto h-full w-full'
-            id={div}>
+              {
+                type==='Pie' || type === 'Bar' ? 
+                <div>
+                  <img src={gear} onClick={changeType} className="px-2 py-2 w-10 hover:cursor-pointer hover:animate-spin" alt="..."></img>
+                </div>
+                : <></>
+              }
               {(chartData === undefined || chartData.length === 0) ? 
-                <h3 className='text-center mt-36'>No Data To Display for <span className='italic font-normal'>{title}</span></h3> 
-              : <></>}
-          </div>    
+                    <h3 className='text-center mt-36'>No Data To Display for <span className='italic font-normal'>{title}</span></h3> 
+                  : <></>}
+              <div className='inline-block mx-auto h-full w-full' id={divID}>
+                  
+              </div>    
         </div>
   );
 }
