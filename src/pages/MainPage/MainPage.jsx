a83ed27406fcd57dadd751bbddbaf7f8b1c15b5c
m4dj1 <59739452+M4dj1@users.noreply.github.com>
2022-09-07 17:26:58 +0200

    [Finalization Weekly Report]

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -12,10 +12,11 @@ export const MainPage = () =>{
                 <div className="flex flex-wrap">
 
                     <div className="w-1/4 px-8">
-                        <Link className="no-underline text-header" to="daily">
+                        <Link className="no-underline text-header" to="">
                             <div className="bg-gray-200 h-60 w-full p-4 text-center rounded-lg shadow-md mb-3 transform transition duration-200 ease-out hover:scale-105 hover:shadow-lg">
                                 <h3>BO Daily Report</h3>
                                 <p>Automatically Generate a back office daily report for a specific day</p>
+                                <h5>Under Test</h5>
                             </div>
                         </Link>
                     </div>
