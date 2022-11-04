5df102dfd5a0ccbacd0c54ab945d30cbae47c4c1
amadji <madji.abd-errahmane@smartest.dz>
2022-11-04 17:41:36 +0100

    [ADD] Welcom Page UI

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -15,13 +15,15 @@ export const MainPage = () => {
         <div className="flex sticky top-40 justify-center items-center">
         <div class="pt-24">
       <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+      <div className="sticky rounded-xl bg-gray-400 w-audo h-auto ">
         <div class="flex flex-col w-full justify-center items-start text-center md:text-left">
-          <h1 class="my-4 text-5xl font-bold leading-tight">
+          <h1 class="my-4 mx-9 text-5xl font-bold leading-tight">
             Welcome {user.name} !
           </h1>
           <p class="leading-normal text-2xl mb-8">
           Here is a set of protocols and tools designed to extract data into custom reports for more automated and efficient reporting and analysis !
           </p>
+        </div>
         </div>
       </div>
     </div>
