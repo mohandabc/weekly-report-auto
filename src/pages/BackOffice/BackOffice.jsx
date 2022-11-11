8bd0a5250d75ac58b6ac9b750e5615be85086b7e
amadji <madji.abd-errahmane@smartest.dz>
2022-11-11 17:24:09 +0100

    [ADD] grid cells

--- src/pages/BackOffice/BackOffice.jsx
+++ src/pages/BackOffice/BackOffice.jsx
@@ -77,6 +77,31 @@ export const BackOffice = () => {
               </div>
             </Link>
           </div>
+
+          <div
+            className={`w-1/4 px-8 block duration-700 relative transform transition-all ease-out
+                    ${
+                      animation
+                        ? "opacity-100 translate-y-0"
+                        : "opacity-0 translate-y-12"
+                    }`}
+          >
+            <Link
+              className="no-underline text-header"
+              to=""
+              style={{ textDecoration: "none", color: "#000" }}
+            >
+              <div className="bg-gray-400 h-60 w-full p-4 text-center rounded-lg shadow-md mb-3 transform transition duration-200 ease-out hover:scale-105 hover:shadow-lg">
+                <h3>BO Monthly Report</h3>
+                <p>
+                  Automatically Generate a back office monthly report for a
+                  specific week
+                </p>
+                <h5>Under Construction</h5>
+              </div>
+            </Link>
+          </div>
+
         </div>
       </header>
     </div>
--- src/pages/FrontOffice/FrontOffice.jsx
+++ src/pages/FrontOffice/FrontOffice.jsx
@@ -46,7 +46,7 @@ export const FrontOffice = () => {
             <Link
               className="no-underline text-header"
               to=""
-              style={{ textDecoration: "none" }}
+              style={{ textDecoration: "none", color: "#000" }}
             >
               <div className="bg-gray-400 h-60 w-full p-4 text-center rounded-lg shadow-md mb-3 transform transition duration-200 ease-out hover:scale-105 hover:shadow-lg">
                 <h3>FO Daily Report</h3>
@@ -69,12 +69,36 @@ export const FrontOffice = () => {
             <Link
               className="no-underline text-header"
               to=""
-              style={{ textDecoration: "none" }}
+              style={{ textDecoration: "none", color: "#000" }}
             >
               <div className="bg-gray-400 h-60 w-full py-4 text-center rounded-lg shadow-md mb-3 transform transition duration-200 ease-out hover:scale-105 hover:shadow-lg">
                 <h3>FO Weekly Report</h3>
                 <p>
                   Automatically Generate a front office weekly report for a
+                  specific week
+                </p>
+                <h5>Under Construction</h5>
+              </div>
+            </Link>
+          </div>
+
+          <div
+            className={`w-1/4 px-8 block duration-700 relative transform transition-all ease-out
+                    ${
+                      animation
+                        ? "opacity-100 translate-y-0"
+                        : "opacity-0 translate-y-12"
+                    }`}
+          >
+            <Link
+              className="no-underline text-header"
+              to=""
+              style={{ textDecoration: "none", color: "#000" }}
+            >
+              <div className="bg-gray-400 h-60 w-full py-4 text-center rounded-lg shadow-md mb-3 transform transition duration-200 ease-out hover:scale-105 hover:shadow-lg">
+                <h3>FO Monthly Report</h3>
+                <p>
+                  Automatically Generate a front office monthly report for a
                   specific week
                 </p>
                 <h5>Under Construction</h5>
