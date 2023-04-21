41168a76efeea830c7ef82ccc2594b4a8b79032d
amadji <madji.abd-errahmane@smartest.dz>
2023-04-21 14:56:36 +0100

    [EDIT] Text area enhancements in the Paragraphe component

--- src/components/Paragraphe/Paragraphe.jsx
+++ src/components/Paragraphe/Paragraphe.jsx
@@ -24,37 +24,39 @@ export const Paragraphe = (props) => {
 
   return (
     <div className="border border-gray-400 rounded p-4">
-    <div className="flex justify-between items-center mb-4">
+    <div className="flex justify-between items-center">
       <h2 className="text-lg font-medium">{props.title}</h2>
       <button
-        className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full border-2 border-purple-600"
+        className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full border-2 border-purple-600 w-1/6"
         onClick={handleEditClick}
       >
         Edit
       </button>
     </div>
     {editing ? (
-      <div>
+      <div className="flex flex-col items-center">
         <textarea
-          className="border border-gray-400 rounded w-full mt-4 h-80"
+          className="border border-gray-400 rounded w-full mt-4 h-80 px-2 py-1 resize-none focus:outline-none focus:ring focus:ring-gray-400 focus:border-transparent bg-gradient-to-r from-slate-400 to-slate-300 text-black"
           value={text}
           onChange={handleTextChange}
         />
-        <button
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full border-2 border-green-600"
-          onClick={handleSaveClick}
-        >
-          Save
-        </button>
-        <button
-          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full border-2 border-gray-600"
-          onClick={handleCancelClick}
-        >
-          Cancel
-        </button>
+        <div className="flex justify-between mt-4 w-full">
+          <button
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full border-2 border-green-600 w-1/6 mx-2"
+            onClick={handleSaveClick}
+          >
+            Save
+          </button>
+          <button
+            className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full border-2 border-gray-600 w-1/6 mx-2"
+            onClick={handleCancelClick}
+          >
+            Cancel
+          </button>
+        </div>
       </div>
     ) : (
-      <p className="mt-4">{text.split('\n').map((line, index) => <React.Fragment key={index}>{line}<br/></React.Fragment>)}</p>
+      <p className="mt-4 whitespace-pre-wrap">{text}</p>
     )}
   </div>
 );
