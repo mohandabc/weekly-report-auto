acdea7698fbc353a1d548f6ce801289b058ef198
mohandabc <mohand_abchiche@live.fr>
2023-05-20 17:14:57 +0100

    add standard function to build various elements of the pdf, and build the page

--- src/services/utils.js
+++ src/services/utils.js
@@ -61,9 +61,182 @@ export const createDoc = (size, orientation, margin) =>{
   };
 }
 
+export const buildPageHeader = () =>{
+  return {
+    columns: [{
+        image: SMARTEST_LOGO,
+        absolutePosition: {x:30 ,y: 30},
+        width: 160
+        },{
+        image: SONATRACH_LOGO,
+        absolutePosition: {x:500 ,y: 10},
+        width: 50
+    }],
+    margin:[0,0,0,80]
+  };
+}
+export const buildPageFooter = (leftText, rightText) =>{
+  return {
+    columns: [{
+      text : leftText,
+      fontSize : 12,
+      alignment:'left',
+      color:'#090909',
+      bold: true,
+      absolutePosition: {x: 40, y: 800},
+        },{
+          text : rightText,
+          fontSize : 12,
+          margin:[0,0,25,0],
+          alignment:'left',
+          color:'#090909',
+          bold: true,absolutePosition: {x: 500,y: 800},
+        }],
+  };
+}
+export const buildTitle = (level, text, isTocItem=true) =>{
+  return {
+    text:text,
+    fontSize : level === 1 ? 18 : 14,
+    margin:level===1? [10, 0, 0, 10] : [20,0,0, 15],
+    color:'#c00000',
+    decoration: 'underline',
+    tocItem: isTocItem
+  }
+}
+export const buildChart = (chart, size) => {
+
+  if (chart === undefined) return null
+  return {
+    image:chart,
+    margin : [0,0,0,10],
+    width : size,
+    alignment:'center',
+  }
+}
+export const buildParagraph = (text, options = {fontSize:10, margin:[30,0,0,0], color:'#000'}) => {
+  return {
+    text:text,
+    fontSize : options.fontSize,
+    margin:options.margin,
+    color: options.color,
+  }
+}
+
+const computeTableColRowRatio = (length) =>{
+  if(length % 2 !== 0) return 1;
+
+  let PGCD = 2;
+  for(let i=2; i < length/2; i++){
+    if(length % i !== 0) continue;
+    if (length/i < i) return PGCD;
+    PGCD = i;
+  }
+}
+/**
+ * When we have an object of key values and we want to display them in a table
+ * we will create a table that has one row, or split the values to create
+ * a more compact table
+ * the split should be done dynamically depending on the number of elements
+ * @param {*} data data of table that consist of only one row, or an object of key values
+ */
+const buildOneRowTableBody = (data) =>{
+  if (data.length <= 0) return []
+  const keys = Object.keys(data[0]);
+  const values = Object.values(data[0]);
+  const ratio = computeTableColRowRatio(keys.length);
+
+  let tableBody = []
+  for(let i=0; i<keys.length; i+=ratio){
+    let row = []
+    for(let j=i; j < i+ratio; j++){
+      row.push(keys[j]);
+      row.push(values[j]);
+    }
+    tableBody.push(row);
+  }
+  return tableBody;
+}
+const getTableHeaders = (row) => {
+  return Object.keys(row)
+}
+const buildSimpleTableBody = (data) =>{
+  if (data.length <= 0) return []
+
+  let tableBody = []
+  const headers = getTableHeaders(data[0])
+  tableBody.push(headers)
+
+  data.forEach(row => {
+    tableBody.push(Object.values(row));
+  })
+  return tableBody;
+}
+
+const buildGroupedTableBody = (data) => {
+  if (data.length <= 1) return []
+  let tableBody = [];
+  const headers = getTableHeaders(data[1]);
+  tableBody.push(headers);
+
+  data.forEach(row => {
+    if(row.title !== undefined) tableBody.push([{'colSpan':headers.length, 'text':row.title, alignment : 'center'}]);
+    else tableBody.push(Object.values(row));
+  })
+  return tableBody;
+}
+export const buildTable = (data, type='simple') => {
+
+  const tableBuilders = {
+    'simple' : buildSimpleTableBody,
+    'one_row' : buildOneRowTableBody,
+    'grouped' : buildGroupedTableBody,
+  }
+
+  const table = {
+    width: 'auto',
+    table : {
+      headerRows:1,
+      body:tableBuilders[type](data)//type==='simple' ? buildSimpleTableBody(data) : buildOneRowTableBody(data),
+      },
+    layout: tablesLayouts[type]
+  }
+
+  return {
+    // A workaround to center tables
+    columns: [
+      { width: '*', text: '' },
+      table,
+      { width: '*', text: '' }
+    ],
+    margin:[0,0,0,20]
+  }
+
+}
+
+
+export const addElementToDoc = (doc, element, pageBreak = null) => {
+  if (element === null || element === undefined) return
+  doc.content.push({...element, pageBreak:pageBreak===null ? 'None':pageBreak});
+}
+
+export const createPage = (doc, content, report, pageNumber, totalPageNumber) =>{
+  let header = buildPageHeader()
+  let footer = buildPageFooter(`${report}`, `Page ${pageNumber} / ${totalPageNumber}`)
+
+  addElementToDoc(doc, header)
+
+  content.forEach(element => {
+    addElementToDoc(doc, element)
+  })
+  addElementToDoc(doc, footer, pageNumber===totalPageNumber?null:'after')
+}
+
 export const downloadPDF = (doc, title) =>{
   pdfMake.createPdf(doc).download(`${title}.pdf`);
 }
+
+// ------------------------------------------- OLD FUNCTION RELATED TO DAILY AND WEEKLY--------------------------
 
 export const pushTabToDoc = (doc, data, columns, widths) => {
   return doc.content.push({
