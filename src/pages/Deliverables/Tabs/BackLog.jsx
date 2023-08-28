91834b9b6eee8d9caeed96affa28553be35f6ae4
Madji Abd-Errahmane <madji.abd-errahmane@smartest.dz>
2023-08-28 09:24:52 +0100

    [ADD] implementing a dynamic way of fetching the data

--- src/pages/Deliverables/Tabs/BackLog.jsx
+++ src/pages/Deliverables/Tabs/BackLog.jsx
@@ -5,15 +5,66 @@ import { BACK_URL } from "../../../constants/URI";
 import { MaterialReactTable } from "material-react-table";
 
 export const BackLog = () => {
-  const [animation, setAnimation] = useState(false);
+  //data and fetching state
   const [data, setData] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isRefetching, setIsRefetching] = useState(false);
+  const [rowCount, setRowCount] = useState(0);
+
+  //table state
+  const [columnFilters, setColumnFilters] = useState([]);
+  const [globalFilter, setGlobalFilter] = useState('');
+  const [sorting, setSorting] = useState([]);
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 5,
+  });
+
+  const [animation, setAnimation] = useState(false);
 
   useEffect(() => {
-    setAnimation(true);
-    getData(BACK_URL, "fetchDeliverables/", 0).then((res) => {
-      setData(res);
-    });
-  }, []);
+    const fetchData = async () => {
+      if (!data.length) {
+        setIsLoading(true);
+      } else {
+        setIsRefetching(true);
+      }
+
+      const url = new URL('fetchDeliverables/', BACK_URL);
+      url.searchParams.set(
+        'start',
+        `${pagination.pageIndex * pagination.pageSize}`,
+      );
+      url.searchParams.set('size', `${pagination.pageSize}`);
+      url.searchParams.set('filters', JSON.stringify(columnFilters ?? []));
+      url.searchParams.set('globalFilter', globalFilter ?? '');
+      url.searchParams.set('sorting', JSON.stringify(sorting ?? []));
+
+      try {
+        const response = await fetch(url.href);
+        const json = await response.json();
+        console.log(json)
+        setData(json);
+        setAnimation(true)
+        // setRowCount(json.meta.totalRowCount);
+      } catch (error) {
+        setIsError(true);
+        console.error(error);
+        return;
+      }
+      setIsError(false);
+      setIsLoading(false);
+      setIsRefetching(false);
+    };
+    fetchData();
+  }, [
+    columnFilters,
+    globalFilter,
+    pagination.pageIndex,
+    pagination.pageSize,
+    sorting,
+  ]);
 
   const columns = useMemo(
     () => [
@@ -60,7 +111,6 @@ export const BackLog = () => {
               }`}
             >
               Deliverables Back Log
-              {console.log(data)}
             </h1>
           </div>
         </div>
@@ -102,6 +152,15 @@ export const BackLog = () => {
               sorting: [{ id: "analysis.create_date", desc: false }],
             }}
             enableDensityToggle={false}
+            state={{
+              columnFilters,
+              globalFilter,
+              isLoading,
+              pagination,
+              showAlertBanner: isError,
+              showProgressBars: isRefetching,
+              sorting,
+            }}
           />
         </div>
       </div>
