4611ca9d66ecf8eca96f84fb8c710910d7479453
mohandabc <mohand_abchiche@live.fr>
2023-08-29 11:05:44 +0100

    add back URI

---
+++ src/constants/URI.js
@@ -0,0 +1,8 @@
+export const API_URL = 'http://10.171.59.80:8069/';
+export const AUTH_URL = 'http://10.171.59.80:8069/api/reports/authenticate';
+// TODO : change BACK_URI after deployment
+export const BACK_URL = 'http://10.171.59.167:8000/'; 
+
+export const db_80 = 'TeamSpace_Prod_2023'
+export const db_staging = 'prod_04_07_2023'
+export const db = db_80
