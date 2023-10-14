ad91c7bc2cca39d0de78e5ddf7fba13f0ba89342
Madji Abd-Errahmane <madji.abd-errahmane@smartest.dz>
2023-10-14 10:33:03 +0100

    [ADD] tabls to the weekly performance page

--- src/pages/FrontOffice/WeeklyPerformance.jsx
+++ src/pages/FrontOffice/WeeklyPerformance.jsx
@@ -1,7 +1,12 @@
+import "./react-tabs.css";
 import { WeeklyPerformanceInputScreen } from "../../components";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { darkModeState } from "../../shared/globalState";
+import { useRecoilValue } from "recoil";
 
 export const WeeklyPerformance = () => {
+  const darkMode = useRecoilValue(darkModeState);
   const [weeklyPerformanceData, setWeeklyPerformanceData] = useState(undefined);
 
   useEffect(() => {
@@ -9,8 +14,41 @@ export const WeeklyPerformance = () => {
   }, [weeklyPerformanceData]);
   return (
     <div className="App">
-      <WeeklyPerformanceInputScreen setWeeklyPerformanceData={setWeeklyPerformanceData}/>
-      {weeklyPerformanceData?'There is data !!':'There is no data !!'}
+      <WeeklyPerformanceInputScreen
+        setWeeklyPerformanceData={setWeeklyPerformanceData}
+      />
+      {weeklyPerformanceData ? (
+        <div id="result-section" className={`bg-slate-300 dark:bg-zinc-900`}>
+          <div className="p-10">
+            <Tabs
+              defaultIndex={0}
+              selectedTabClassName={`${
+                darkMode
+                  ? "react-tabs__tab--selecteddark"
+                  : "react-tabs__tab--selectedlight"
+              }`}
+            >
+              <TabList>
+                <Tab>Event KPI's</Tab>
+                <Tab>Tripping Speed</Tab>
+                <Tab>Drill State</Tab>
+                <Tab>Monitoring KPI</Tab>
+                <Tab>NPT Analysis</Tab>
+              </TabList>
+
+              <TabPanel></TabPanel>
+
+              <TabPanel></TabPanel>
+
+              <TabPanel></TabPanel>
+              <TabPanel></TabPanel>
+              <TabPanel></TabPanel>
+            </Tabs>
+          </div>
+        </div>
+      ) : (
+        "There is no data !!"
+      )}
     </div>
   );
 };
