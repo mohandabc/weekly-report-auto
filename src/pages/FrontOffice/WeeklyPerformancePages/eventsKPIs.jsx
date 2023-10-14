f2978418532c613268ee9d3e9c556f1992b708af
Madji Abd-Errahmane <madji.abd-errahmane@smartest.dz>
2023-10-14 16:30:15 +0100

    [ADD] separator

--- src/pages/FrontOffice/WeeklyPerformancePages/eventsKPIs.jsx
+++ src/pages/FrontOffice/WeeklyPerformancePages/eventsKPIs.jsx
@@ -160,15 +160,17 @@ export const EventsKPIs = (eventsKPI) => {
 
   return (
     <div
-      className="sticky rounded-xl bg-gray-200 dark:bg-stone-700 h-auto p-10"
-      style={{ height: 1250, width: "100%" }}
+      className="sticky rounded-xl bg-gray-200 dark:bg-stone-700 h-auto px-10"
+      style={{ height: 1310, width: "100%" }}
     >
+      <div className="flex justify-center items-center">
+        <span className="text-xl py-4">Drilling Events</span>
+      </div>
       <div>
         <MaterialReactTable
           columns={columnsEvents}
           data={eventsKPI.eventsKPI["event_kpi_res"]}
           initialState={{ density: "compact", showColumnFilters: true }}
-          manualSorting
           muiTableHeadCellProps={{
             sx: {
               backgroundColor: darkMode
@@ -208,51 +210,52 @@ export const EventsKPIs = (eventsKPI) => {
           }}
         />
       </div>
-      <div className="mt-10">
-        <MaterialReactTable
-          columns={columnsEventsNPT}
-          data={eventsKPI.eventsKPI["event_caused_npt_res"]}
-          initialState={{ density: "compact", showColumnFilters: true }}
-          manualSorting
-          muiTableHeadCellProps={{
-            sx: {
-              backgroundColor: darkMode
-                ? "rgba(0, 0, 0, 0.4)"
-                : "rgba(39, 73, 98, 0.3)",
-              color: darkMode ? "rgba(28, 28, 26, 1)" : "rgba(55, 90, 112, 1)",
-            },
-          }}
-          muiTableBodyRowProps={({ row }) => ({
-            sx: {
-              cursor: "pointer",
-              backgroundColor: (theme) =>
-                row.index % 2 !== 0
-                  ? darkMode
-                    ? "rgba(28, 28, 26, 0.2)"
-                    : "rgba(55, 90, 112, 0.15)"
-                  : darkMode
-                  ? "rgb(42, 44, 41, 0.2)"
-                  : "rgba(65, 100, 122, 0.1)",
-            },
-          })}
-          muiBottomToolbarProps={{
-            sx: {
-              backgroundColor: darkMode
-                ? "rgba(0, 0, 0, 0.4)"
-                : "rgba(39, 73, 98, 0.3)",
-              color: darkMode ? "rgba(28, 28, 26, 1)" : "rgba(55, 90, 112, 1)",
-            },
-          }}
-          muiTopToolbarProps={{
-            sx: {
-              backgroundColor: darkMode
-                ? "rgba(0, 0, 0, 0.4)"
-                : "rgba(39, 73, 98, 0.3)",
-              color: darkMode ? "rgba(28, 28, 26, 1)" : "rgba(55, 90, 112, 1)",
-            },
-          }}
-        />
+
+      <div className="flex justify-center items-center">
+        <span className="text-xl py-4">Drilling Events That Caused NPT</span>
       </div>
+      <MaterialReactTable
+        columns={columnsEventsNPT}
+        data={eventsKPI.eventsKPI["event_caused_npt_res"]}
+        initialState={{ density: "compact", showColumnFilters: true }}
+        muiTableHeadCellProps={{
+          sx: {
+            backgroundColor: darkMode
+              ? "rgba(0, 0, 0, 0.4)"
+              : "rgba(39, 73, 98, 0.3)",
+            color: darkMode ? "rgba(28, 28, 26, 1)" : "rgba(55, 90, 112, 1)",
+          },
+        }}
+        muiTableBodyRowProps={({ row }) => ({
+          sx: {
+            cursor: "pointer",
+            backgroundColor: (theme) =>
+              row.index % 2 !== 0
+                ? darkMode
+                  ? "rgba(28, 28, 26, 0.2)"
+                  : "rgba(55, 90, 112, 0.15)"
+                : darkMode
+                ? "rgb(42, 44, 41, 0.2)"
+                : "rgba(65, 100, 122, 0.1)",
+          },
+        })}
+        muiBottomToolbarProps={{
+          sx: {
+            backgroundColor: darkMode
+              ? "rgba(0, 0, 0, 0.4)"
+              : "rgba(39, 73, 98, 0.3)",
+            color: darkMode ? "rgba(28, 28, 26, 1)" : "rgba(55, 90, 112, 1)",
+          },
+        }}
+        muiTopToolbarProps={{
+          sx: {
+            backgroundColor: darkMode
+              ? "rgba(0, 0, 0, 0.4)"
+              : "rgba(39, 73, 98, 0.3)",
+            color: darkMode ? "rgba(28, 28, 26, 1)" : "rgba(55, 90, 112, 1)",
+          },
+        }}
+      />
     </div>
   );
 };
